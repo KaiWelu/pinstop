2f9b8cc8afa8273402ea6842ffbff935d42dc50c
KaiWelu <kaiweluda@gmail.com>
2024-02-08 15:02:33 +0100

    feat: adds context basic setup

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
-import { IContextType } from "@/types";
+import { getCurrentUser } from "@/lib/appwrite/api";
+import { IContextType, IUser } from "@/types";
 import { createContext, useContext, useEffect, useState } from "react";
 
 export const INITIAL_USER = {
@@ -21,8 +22,43 @@ const INITIAL_STATE = {
 
 const AuthContext = createContext<IContextType>(INITIAL_STATE);
 
-const AuthProvider = () => {
-  return <div>AuthContext</div>;
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<IUser>(INITIAL_USER);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const checkAuthUser = async () => {
+    try {
+      const currentAccount = await getCurrentUser();
+
+      if (currentAccount) {
+        setUser({
+          id: currentAccount.$id,
+          name: currentAccount.name,
+          username: currentAccount.username,
+          email: currentAccount.email,
+          imageUrl: currentAccount.imageUrl,
+          bio: currentAccount.bio,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      return false;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const value = {
+    user,
+    setUser,
+    isLoading,
+    isAuthenticated,
+    setIsAuthenticated,
+    checkAuthUser,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export default AuthContext;
