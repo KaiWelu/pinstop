05000ea553dee14f77891ff644ff1d78cd209759
KaiWelu <kaiweluda@gmail.com>
2024-02-13 22:52:55 +0100

    feat: adds constants

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { useUserContext } from "@/context/AuthContext";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const LeftSidebar = () => {
-  return <div>LeftSidebar</div>;
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    if (isSuccess) {
+      // this may need to be changed to navigate(0) <-- dunno what it means yet
+      navigate("/sign-in");
+    }
+  }, [isSuccess]);
+
+  return (
+    <nav className="leftsidebar">
+      <div className="flex flex-col gap-11">
+        <Link to="/" className="flex gap-3 items-center">
+          <img
+            src="/public/assets/images/logo.svg"
+            alt="logo"
+            width={170}
+            height={63}
+          />
+        </Link>
+        <Link to={"/profile/${user.id}"} className="flex gap-3 items-center">
+          <img
+            src={user.imageUrl || "/public/assets/images/profile.png"}
+            alt="profile"
+            className="h-14 w-14 rounded-full"
+          />
+          <div className="flex flex-col">
+            <p className="body-bold">{user.name}</p>
+            <p className="small-regular text-light-3">@{user.username}</p>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default LeftSidebar;
---
+++ src/constants/index.ts
@@ -0,0 +1,52 @@
+//this file is for static code. it will be exported to make the code in other parts of the app more structured
+
+export const sidebarLinks = [
+  {
+    imgURL: "/assets/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/icons/wallpaper.svg",
+    route: "/explore",
+    label: "Explore",
+  },
+  {
+    imgURL: "/assets/icons/people.svg",
+    route: "/all-users",
+    label: "People",
+  },
+  {
+    imgURL: "/assets/icons/bookmark.svg",
+    route: "/saved",
+    label: "Saved",
+  },
+  {
+    imgURL: "/assets/icons/gallery-add.svg",
+    route: "/create-post",
+    label: "Create Post",
+  },
+];
+
+export const bottombarLinks = [
+  {
+    imgURL: "/assets/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/icons/wallpaper.svg",
+    route: "/explore",
+    label: "Explore",
+  },
+  {
+    imgURL: "/assets/icons/bookmark.svg",
+    route: "/saved",
+    label: "Saved",
+  },
+  {
+    imgURL: "/assets/icons/gallery-add.svg",
+    route: "/create-post",
+    label: "Create",
+  },
+];
