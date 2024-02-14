a0d7f9356470c4158bb9fd2eaf5292eaccbf0392
KaiWelu <kaiweluda@gmail.com>
2024-02-14 16:48:25 +0100

    feat: adds create post page

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { bottombarLinks } from "@/constants";
+import { Link, useLocation } from "react-router-dom";
 
 const Bottombar = () => {
-  return <div>Bottombar</div>;
+  const { pathname } = useLocation();
+  return (
+    <section className="bottom-bar">
+      {bottombarLinks.map((link) => {
+        // this is a bool related to the actual pathname of a link
+        const isActive = pathname === link.route;
+        // please note the group and group:hover + the temporal strings
+        return (
+          <Link
+            to={link.route}
+            className={`${
+              isActive && "bg-primary-500 rounded-[10px]"
+            } flex-center flex-col gap-1 p-2 transition`}
+            key={link.label}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              className={`${isActive && "invert-white"}`}
+              width={16}
+              height={16}
+            />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Bottombar;
