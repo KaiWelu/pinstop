a1ec674db2c339b0df2c0745c076203fcdddeb36
KaiWelu <kaiweluda@gmail.com>
2024-02-20 23:15:17 +0100

    feat: sets up handling of likes and saves as placeholder functions

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,4 +1,12 @@
+import { useUserContext } from "@/context/AuthContext";
+import {
+  useDeleteSavePost,
+  useLikePosts,
+  useSavePost,
+} from "@/lib/react-query/queriesAndMutations";
+import { checkIsLiked } from "@/lib/utils";
 import { Models } from "appwrite";
+import { useState, useEffect } from "react";
 
 type PostStatsProps = {
   post: Models.Document;
@@ -6,26 +14,48 @@ type PostStatsProps = {
 };
 
 const PostStats = ({ post, userId }: PostStatsProps) => {
+  // this creates a list of likes from users for the current post
+  const likesList = post.likes.map((user: Models.Document) => user.$id);
+
+  const [likes, setLikes] = useState(likesList);
+  const [isSaved, setIsSaved] = useState(false);
+
+  const { mutate: likePost } = useLikePosts();
+  const { mutate: savePost } = useSavePost();
+  const { mutate: deleteSavedPost } = useDeleteSavePost();
+  const { data: currentUser } = useUserContext();
+
+  const handleLikePost = () => {};
+  const handleSavePost = () => {};
+
   return (
     <div className="flex justify-between items-center z-20">
       <div className="flex gap-2 mr-5">
         <img
-          src="/public/assets/icons/like.svg"
+          src={`${
+            checkIsLiked(likes, userId)
+              ? "/public/assets/icons/liked.svg"
+              : "/public/assets/icons/like.svg"
+          }`}
           alt="like"
           width={20}
           height={20}
-          onClick={() => {}}
+          onClick={handleLikePost}
           className="cursor-pointer"
         />
-        <p className="small-medium lg:base-medium">0</p>
+        <p className="small-medium lg:base-medium">{likes.length}</p>
       </div>
       <div className="flex gap-2">
         <img
-          src="/public/assets/icons/save.svg"
+          src={`${
+            isSaved
+              ? "/public/assets/icons/saved.svg"
+              : "/public/assets/icons/save.svg"
+          }`}
           alt="like"
           width={20}
           height={20}
-          onClick={() => {}}
+          onClick={handleSavePost}
           className="cursor-pointer"
         />
       </div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -27,3 +27,7 @@ export function formatDate(dateString: string): string {
     return `${secondsDifference} second${secondsDifference > 1 ? "s" : ""} ago`;
   }
 }
+
+export const checkIsLiked = (likeList: string[], userId: string) => {
+  return likeList.includes(userId);
+};
