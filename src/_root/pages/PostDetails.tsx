7b3eb7e00e01ae05fa7efc7f9a752ccbb1046329
KaiWelu <kaiweluda@gmail.com>
2024-03-01 16:29:35 +0100

    end of day

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,4 +1,6 @@
 import Loader from "@/components/shared/Loader";
+import { Button } from "@/components/ui/button";
+import { useUserContext } from "@/context/AuthContext";
 import { useGetPostById } from "@/lib/react-query/queriesAndMutations";
 import { formatDate } from "@/lib/utils";
 import { Link, useParams } from "react-router-dom";
@@ -6,8 +8,11 @@ import { Link, useParams } from "react-router-dom";
 const PostDetails = () => {
   const { id } = useParams();
   const { data: post, isPending } = useGetPostById(id || "");
+  const { user } = useUserContext();
 
   const dateFormatted = formatDate(post?.$createdAt);
+
+  const handleDeletePost = () => {};
 
   return (
     <div className="post_details-container">
@@ -15,29 +20,64 @@ const PostDetails = () => {
         <Loader />
       ) : (
         <div className="post_details-card">
-          <div className="flex items-center gap-3">
-            <Link to={`/profile/${post?.creator.$id}`}>
-              <img
-                src={
-                  post?.creator.imageUrl || "/public/assets/images/profile.png"
-                }
-                alt="creator"
-                className="rounded-full w-12 lg:h-12"
-              />
-            </Link>
-            <div className="flex flex-col">
-              <p className="base-medium lg:body-bold text-light-1">
-                {post?.creator.name}
-              </p>
-              <div className="flex-center gap-2 text-light-3">
-                <p className="subtle-semi-bold lg:small-regular">
-                  {dateFormatted}
-                </p>{" "}
-                -{" "}
-                <p className="subtle-semi-bold lg:small-regular">
-                  {" "}
-                  {post?.location}{" "}
-                </p>
+          <img src={post?.imageUrl} alt="post" className="post_details-img" />
+
+          <div className="post_details-info">
+            <div className="flex-between w-full">
+              <Link
+                to={`/profile/${post?.creator.$id}`}
+                className="flex items-center gap-3"
+              >
+                <img
+                  src={
+                    post?.creator.imageUrl ||
+                    "/public/assets/images/profile.png"
+                  }
+                  alt="creator"
+                  className="rounded-full w-8 h-8 lg:w-12 lg:h-12"
+                />
+                <div className="flex flex-col">
+                  <p className="base-medium lg:body-bold text-light-1">
+                    {post?.creator.name}
+                  </p>
+                  <div className="flex-center gap-2 text-light-3">
+                    <p className="subtle-semi-bold lg:small-regular">
+                      {dateFormatted}
+                    </p>
+                    -
+                    <p className="subtle-semi-bold lg:small-regular">
+                      {post?.location}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+
+              <div className="flex-center">
+                <Link
+                  to={`/update-post/${post?.$id}`}
+                  className={`${user.id !== post?.creator.$id && "hidden"}`}
+                >
+                  <img
+                    src="/public/assets/icons/edit.svg"
+                    width={24}
+                    height={24}
+                    alt="edit"
+                  />
+                </Link>
+                <Button
+                  onClick={handleDeletePost}
+                  variant="ghost"
+                  className={`ghost_details-delete_btn ${
+                    user.id !== post?.creator.$id && "hidden"
+                  }`}
+                >
+                  <img
+                    src="/public/assets/icons/delete.svg"
+                    width={24}
+                    height={24}
+                    alt="delete"
+                  />
+                </Button>
               </div>
             </div>
           </div>
