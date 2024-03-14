90d5bf7606f7e869fa489a7316c36d456f223ff4
KaiWelu <kaiweluda@gmail.com>
2024-03-14 11:15:09 +0100

    feat: sets up the layout of the saved posts page

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -1,7 +1,22 @@
+import { useGetCurrentUser } from "@/lib/react-query/queriesAndMutations";
+
 const Saved = () => {
+  const user = useGetCurrentUser();
+  console.log(user);
+
   return (
-    <div className="flex items-center justify-center w-full">
-      Nothing to see here yet...
+    <div className="explore-container">
+      <div className="explore-inner_container">
+        <h2 className="h3-bold md:h2-bold w-full flex flex-row gap-2 items-center">
+          <img
+            src="/assets/icons/save.svg"
+            alt="save"
+            className="md:w-8 md:h-8 w-6 h-6"
+          />
+          Saved Posts
+        </h2>
+      </div>
+      <div className="">Das ist ein Test</div>
     </div>
   );
 };
