e787f71def5333e306d5ae0880839149395830aa
KaiWelu <kaiweluda@gmail.com>
2024-03-15 17:08:38 +0100

    feat: adds hearts to usernames and a message if nobody liked a post yet

--- src/_root/pages/LikedPosts.tsx
+++ src/_root/pages/LikedPosts.tsx
@@ -1,32 +1,51 @@
+import Loader from "@/components/shared/Loader";
 import { useGetPostById } from "@/lib/react-query/queriesAndMutations";
 import { Models } from "appwrite";
-import { useParams } from "react-router-dom";
+import { Http2ServerRequest } from "http2";
+import { Link, useParams } from "react-router-dom";
 
 const LikedPosts = () => {
   const { id } = useParams();
   const { data: post, isPending } = useGetPostById(id || "");
   console.log(post);
 
+  // if the details are still loading display a Loader component
+  if (isPending) return <Loader />;
+
   return (
     <div className="explore-container">
       <div className="explore-inner_container">
-        <h2 className="h3-bold md:h2-bold w-full flex flex-row gap-2 items-center">
-          {post?.likes.length} people liked this post!
-        </h2>
+        {post?.likes.length > 0 ? (
+          <h2 className="h3-bold md:h2-bold w-full flex flex-row gap-2 items-center">
+            {post?.likes.length} people liked this post!
+          </h2>
+        ) : (
+          <h2 className="h3-bold md:h2-bold w-full flex flex-row gap-2 items-center">
+            Nobody liked this post... yet!
+          </h2>
+        )}
       </div>
-      <div className="flex flex-wrap gap-9 w-full max-w-5xl mt-7">
+      <div className="flex flex-wrap gap-2 w-full max-w-5xl mt-7">
         <ul className="flex flex-col gap-3">
           {post?.likes.map((item: Models.Document) => {
             return (
-              <li className="flex flex-1 items-center gap-4">
+              <li className="flex flex-1 items-center gap-3">
                 <img
                   src={item.imageUrl}
                   alt="liker"
                   className="rounded-full w-12 lg:h-12"
                 />
-                <p className="base-medium lg:body-bold text-light-1">
-                  {item.name}
-                </p>
+                <Link>
+                  <p className="base-medium lg:body-bold text-light-1">
+                    {item.name}
+                  </p>
+                </Link>
+
+                <img
+                  src="/assets/icons/liked.svg"
+                  alt="heart"
+                  className="lg:h-7 lg:w-7 h-5 w-5"
+                />
               </li>
             );
           })}
